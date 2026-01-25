2fdf7cf52faba9f01c9229868771de657efafb76
qwest <icecoffee2500@gmail.com>
2026-01-26 01:52:18 +0900

    dataset test - no crop scale segfaceceleba

--- utils/transform.py
+++ utils/transform.py
@@ -138,8 +138,8 @@ class TrainTransform:
             [
                 ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5),
                 HorizontalFlip(p=0.5),
-                RandomScale([0.75, 1.0, 1.25, 1.5, 1.75, 2.0]),
-                RandomCrop(image_size),
+                # RandomScale([0.75, 1.0, 1.25, 1.5, 1.75, 2.0]),
+                # RandomCrop(image_size),
                 ToTensor(),
                 Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             ]
