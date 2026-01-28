18c99d06f2febf5864267dd270c464f916be8a48
qwest <icecoffee2500@gmail.com>
2026-01-28 19:40:40 +0900

    test.sh created

--- test.sh
+++ test.sh
@@ -24,10 +24,10 @@ python test.py --weights ./weights/20260128_051904/best_resnet34_global.pt --dev
 python test.py --weights ./weights/20260128_051904/best_resnet34_global.pt --device-id 0 --resolution 192
 python test.py --weights ./weights/20260128_051904/best_resnet34_global.pt --device-id 0 --resolution 128
 
-# FL+MRKD (512 384 256)
-python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 576
-python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 512
-python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 384
-python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 256
-python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 192
-python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 128
+# # FL+MRKD (512 384 256)
+# python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 576
+# python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 512
+# python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 384
+# python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 256
+# python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 192
+# python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 128
