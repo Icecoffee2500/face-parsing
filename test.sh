2e7565e05773504db60d757585764d6d137e34d4
qwest <icecoffee2500@gmail.com>
2026-01-29 12:56:26 +0900

    test.sh modified

--- test.sh
+++ test.sh
@@ -24,26 +24,26 @@
 # python test.py --weights ./weights/20260128_051904/best_resnet34_global.pt --device-id 0 --resolution 192
 # python test.py --weights ./weights/20260128_051904/best_resnet34_global.pt --device-id 0 --resolution 128
 
-# # FL+MRKD (512 384 256)
-# python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 576
-# python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 512
-# python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 384
-# python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 256
-# python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 192
-# python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 128
+# FL+MRKD (512 384 256)
+python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 576
+python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 512
+python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 384
+python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 256
+python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 192
+python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 128
 
-# CL (512 256 128)
-python test.py --weights ./weights/20260128_114239/best_resnet34.pt --device-id 0 --resolution 576
-python test.py --weights ./weights/20260128_114239/best_resnet34.pt --device-id 0 --resolution 512
-python test.py --weights ./weights/20260128_114239/best_resnet34.pt --device-id 0 --resolution 384
-python test.py --weights ./weights/20260128_114239/best_resnet34.pt --device-id 0 --resolution 256
-python test.py --weights ./weights/20260128_114239/best_resnet34.pt --device-id 0 --resolution 192
-python test.py --weights ./weights/20260128_114239/best_resnet34.pt --device-id 0 --resolution 128
+# # CL (512 256 128)
+# python test.py --weights ./weights/20260128_114239/best_resnet34.pt --device-id 0 --resolution 576
+# python test.py --weights ./weights/20260128_114239/best_resnet34.pt --device-id 0 --resolution 512
+# python test.py --weights ./weights/20260128_114239/best_resnet34.pt --device-id 0 --resolution 384
+# python test.py --weights ./weights/20260128_114239/best_resnet34.pt --device-id 0 --resolution 256
+# python test.py --weights ./weights/20260128_114239/best_resnet34.pt --device-id 0 --resolution 192
+# python test.py --weights ./weights/20260128_114239/best_resnet34.pt --device-id 0 --resolution 128
 
-# CL (512 384 256)
-python test.py --weights ./weights/20260128_114422/best_resnet34.pt --device-id 0 --resolution 576
-python test.py --weights ./weights/20260128_114422/best_resnet34.pt --device-id 0 --resolution 512
-python test.py --weights ./weights/20260128_114422/best_resnet34.pt --device-id 0 --resolution 384
-python test.py --weights ./weights/20260128_114422/best_resnet34.pt --device-id 0 --resolution 256
-python test.py --weights ./weights/20260128_114422/best_resnet34.pt --device-id 0 --resolution 192
-python test.py --weights ./weights/20260128_114422/best_resnet34.pt --device-id 0 --resolution 128
+# # CL (512 384 256)
+# python test.py --weights ./weights/20260128_114422/best_resnet34.pt --device-id 0 --resolution 576
+# python test.py --weights ./weights/20260128_114422/best_resnet34.pt --device-id 0 --resolution 512
+# python test.py --weights ./weights/20260128_114422/best_resnet34.pt --device-id 0 --resolution 384
+# python test.py --weights ./weights/20260128_114422/best_resnet34.pt --device-id 0 --resolution 256
+# python test.py --weights ./weights/20260128_114422/best_resnet34.pt --device-id 0 --resolution 192
+# python test.py --weights ./weights/20260128_114422/best_resnet34.pt --device-id 0 --resolution 128
