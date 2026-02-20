a438b331dc651e52e83101b2a8fa356c6fac3e3a
juju0111 <p3549823@gmail.com>
2026-02-20 04:46:43 +0000

    test.sh modified

--- test.sh
+++ test.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# # ViT-FL (512 256 128)
+# python test.py --weights ./weights/20260214_131434/best_vit_small_global.pt --backbone vit_small --device-id 2 --resolution 576
+# python test.py --weights ./weights/20260214_131434/best_vit_small_global.pt --backbone vit_small --device-id 2 --resolution 512
+# python test.py --weights ./weights/20260214_131434/best_vit_small_global.pt --backbone vit_small --device-id 2 --resolution 384
+# python test.py --weights ./weights/20260214_131434/best_vit_small_global.pt --backbone vit_small --device-id 2 --resolution 256
+# python test.py --weights ./weights/20260214_131434/best_vit_small_global.pt --backbone vit_small --device-id 2 --resolution 192
+# python test.py --weights ./weights/20260214_131434/best_vit_small_global.pt --backbone vit_small --device-id 2 --resolution 128
+
+# ViT-FL+MRKD (512 256 128)
+python test.py --weights ./weights/20260214_131657/best_vit_small_global.pt --backbone vit_small --device-id 2 --resolution 576
+python test.py --weights ./weights/20260214_131657/best_vit_small_global.pt --backbone vit_small --device-id 2 --resolution 512
+python test.py --weights ./weights/20260214_131657/best_vit_small_global.pt --backbone vit_small --device-id 2 --resolution 384
+python test.py --weights ./weights/20260214_131657/best_vit_small_global.pt --backbone vit_small --device-id 2 --resolution 256
+python test.py --weights ./weights/20260214_131657/best_vit_small_global.pt --backbone vit_small --device-id 2 --resolution 192
+python test.py --weights ./weights/20260214_131657/best_vit_small_global.pt --backbone vit_small --device-id 2 --resolution 128
+
 # # FL (512 256 128)
 # python test.py --weights ./weights/20260127_121759/best_resnet34_global.pt --device-id 0 --resolution 576
 # python test.py --weights ./weights/20260127_121759/best_resnet34_global.pt --device-id 0 --resolution 512
@@ -24,13 +40,13 @@
 # python test.py --weights ./weights/20260128_051904/best_resnet34_global.pt --device-id 0 --resolution 192
 # python test.py --weights ./weights/20260128_051904/best_resnet34_global.pt --device-id 0 --resolution 128
 
-# FL+MRKD (512 384 256)
-python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 576
-python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 512
-python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 384
-python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 256
-python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 192
-python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 128
+# # FL+MRKD (512 384 256)
+# python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 576
+# python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 512
+# python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 384
+# python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 256
+# python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 192
+# python test.py --weights ./weights/20260128_052100/best_resnet34_global.pt --device-id 0 --resolution 128
 
 # # CL (512 256 128)
 # python test.py --weights ./weights/20260128_114239/best_resnet34.pt --device-id 0 --resolution 576
